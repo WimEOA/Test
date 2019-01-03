4bae0f5fb726b8389c9de4c58f6968c3017993a3
Wim Albersen <w.albersen@eyeonair.eu>
2019-01-03 13:31:04 +0100

    First commit

---
+++ Image2Icon/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace Image2Icon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog {Filter = "Png files|*.png"};
+            if (ofd.ShowDialog() == true)
+            {
+                var file = ofd.FileName;
+                var icoFile = Path.ChangeExtension(file, ".ico");
+                ImagingHelper.ConvertToIcon(file, icoFile);
+            }
+        }
+    }
+}
